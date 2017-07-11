e0af3e6d4233f9e559ab14a11a3643663a025abb
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-11 16:50:24 +0900

    共通インターフェースの追加

---
+++ src/common/Common.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * 比較可能オブジェクトのインターフェース
+ */
+export interface IComparable<T> {
+    /**
+     * オブジェクトと比較するメソッド
+     * @param other 比較対象のオブジェクト
+     * @returns 比較対象の方が大きい場合は正の値、等しい場合は0、小さい場合は負の値を返す
+     */
+    compareTo(other: T): number;
+}
+
+/**
+ * 比較器のインターフェース
+ */
+export interface IComparator<T> {
+    /**
+     * 2つのオブジェクトを比較するメソッド
+     * @param one 一つ目のオブジェクト
+     * @param another もう片方のオブジェクト
+     * @returns 一つ目のオブジェクトに対し、もう片方のオブジェクトの方が大きい場合は正の値、等しい場合は0、小さい場合は負の値を返す
+     */
+    compare(one: T, another: T) : number;
+}
