31e456246df160d755ac45402bda3d1c5eb31397
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-12 11:05:03 +0900

    カード比較器のベースを追加

---
+++ src/logic/CardComparator.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+import { IComparator } from '../common/Common';
+import { Card } from '../model/CardModel';
+
+/**
+ * カード比較器のベース
+ */
+export abstract class CardComparator implements IComparator<Card> {
+    public abstract compare(one: Card, another: Card): number;
+}
+
+// TODO: 強さを元に比較する比較器を作る
+export class CardPowerComparator extends CardComparator {
+    // TODO compareメソッドを何とかする
+}
---
+++ test/logic/CardComparatorTest.ts
@@ -0,0 +1,47 @@
+'use strict';
+
+import * as assert from 'power-assert';
+import { CardPowerComparator } from '../../src/logic/CardComparator';
+import { Card, Trump, JokerTrump, TrumpSuitType } from '../../src/model/CardModel';
+
+describe('カード比較器', () => {
+    const comparator = new CardPowerComparator();
+
+    context('比較', () => {
+        it('スペードの11とハートの13: ハートの13', () => {
+            const card1 = new Trump(TrumpSuitType.Spades, 11);
+            const card2 = new Trump(TrumpSuitType.Hearts, 13);
+            assert.equal(comparator.compare(card1, card2) > 0, true);
+        });
+        it('クラブの2とクラブの7: クラブの2', () => {
+            const card1 = new Trump(TrumpSuitType.Crabs, 2);
+            const card2 = new Trump(TrumpSuitType.Crabs, 7);
+            assert.equal(comparator.compare(card1, card2) < 0, true);
+        });
+        it('ジョーカーとダイヤの1: ジョーカー', () => {
+            const card1 = new JokerTrump();
+            const card2 = new Trump(TrumpSuitType.Diamonds, 1);
+            assert.equal(comparator.compare(card1, card2) < 0, true);
+        });
+        it('ダイヤの3とクラブの3: 同じ', () => {
+            const card1 = new Trump(TrumpSuitType.Diamonds, 3);
+            const card2 = new Trump(TrumpSuitType.Crabs, 3);
+            assert.equal(comparator.compare(card1, card2) == 0, true);
+        });
+    });
+
+    context('並び替え', () => {
+        it('[C3,D5,J,C1,H1] -> [C3,D5,C1,H1,J]', () => {
+            const C3 = new Trump(TrumpSuitType.Crabs, 3);
+            const D5 = new Trump(TrumpSuitType.Diamonds, 5);
+            const J = new JokerTrump();
+            const C1 = new Trump(TrumpSuitType.Crabs, 1);
+            const H1 = new Trump(TrumpSuitType.Hearts, 1);
+
+            const cards: Card[] = [C3,D5,J,C1,H1];
+            cards.sort(comparator.compare);
+            
+            assert.equal(cards, [C3,D5,C1,H1,J]);
+        });
+    });
+});
