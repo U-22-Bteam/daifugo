cb601767aeee43d2e933fd9d127c85d788e50ba0
Ryunosuke Kubo <kbc16a07@stu.kawahara.ac.jp>
2017-07-19 15:13:50 +0900

    修正希望

--- c_src/MatchScreem.js
+++
@@ -1,7 +0,0 @@
-var container = document.getElementById('canvas-container');
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-//親要素のサイズをCanvasに指定
-canvas.width = container.clientWidth;
-canvas.height = container.clientHeight; 
