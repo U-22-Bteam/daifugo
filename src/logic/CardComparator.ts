9ddc967bb9907083d8602614e93ed3afcc6bb626
kt2029 <kbc16a12@stu.kawahara.ac.jp>
2017-07-19 20:39:41 +0900

    CardComparatorの処理を作成

--- src/logic/CardComparator.ts
+++ src/logic/CardComparator.ts
@@ -1,4 +1,3 @@
-'use strict';
 
 import { IComparator } from '../common/Common';
 import { Card } from '../model/CardModel';
@@ -13,4 +12,36 @@ export abstract class CardComparator implements IComparator<Card> {
 // TODO: 強さを元に比較する比較器を作る
 export class CardPowerComparator extends CardComparator {
     // TODO compareメソッドを何とかする
+    /*
+    * 左側が大きい場合に負の数、右側が大きい場合に正の数、等しい場合に0を返す
+    */
+    public compare(one: Card, another: Card){
+        var card1 = one.getCode();
+        var card2 = another.getCode();
+        var strength1 = this.CardCodeToCardStrength(card1);
+        var strength2 = this.CardCodeToCardStrength(card2);
+        if (strength1 > strength2){
+            return -1;
+        }else if (strength1 < strength2){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    /*
+    * カードコードの数字部分を直接比較できるように直す
+    */
+    CardCodeToCardStrength(code: string){
+        var str:string;
+        var strength:number;
+        str = code.substr(1);
+        if (code.charAt(0) === 'J'){
+            strength = 15;
+        }else if (str == 2 || str == 1){
+            strength = str + 13;
+        }else{
+            strength = str - 0;
+        }
+        return strength;
+    }
 }
