b30a094d17977f017ce180e29ae630c8cbe68101
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-19 11:28:26 +0900

    使いそうなモデルの追加

---
+++ src/model/User.ts
@@ -0,0 +1,18 @@
+import * as SocketIO from 'socket.io';
+
+export class User {
+    readonly username: string;
+
+    constructor(username: string) {
+        this.username = username;
+    }
+}
+
+export class ConnectionUser extends User {
+    readonly socket: SocketIO.Socket;
+
+    constructor(username: string, socket: SocketIO.Socket) {
+        super(username);
+        this.socket = socket;
+    }
+}
