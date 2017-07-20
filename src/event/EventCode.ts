150fb53c3ae9520bf0dba691d924986921773c4c
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-20 13:34:22 +0900

    イベントコードの追加

--- src/event/EventCode.ts
+++ src/event/EventCode.ts
@@ -2,12 +2,17 @@
 export class EventCode {
     public static Connect = 'connect';
     public static Disconnect = 'disconnect';
+    
+    public static UserJoin = "user.join";
+    public static UserReady = 'user.ready';
+    public static UserUnready = "user.unready";
+
+    public static AcceptJoin = "accept.join";
+
+    public static GameReady = 'game.ready';
 
     public static UserCardDraw = 'user.card.draw';
     public static UserCardDiscard = 'user.card.discard';
 
-    public static UserJoin = "user.join";
-    public static UserReady = 'user.ready';
-
-    public static GameReady = 'game.ready';
+    public static GameFinish = "game.finish";
 }
