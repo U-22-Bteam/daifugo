59f473352ac2e72daf1b2192257f94a993d13876
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-20 14:14:35 +0900

    Revert "Merge pull request #9 from U-22-Bteam/develop/base"
    This reverts commit 23d639c4d308f552d9076889bcce13928d117616, reversing
    changes made to 4a4f6bfa0eb14360609100b22e527fb2c2e82a46.

--- c_src/MatchScreen.js
+++ c_src/MatchScreen.js
@@ -7,9 +7,9 @@ canvas.width = container.clientWidth;
 canvas.height = container.clientHeight;
 
 var image = new Image();
-image.src = '..\\images\\png\\C01.png';
+image.src = '..\images\png\C01.png';
 // canvas.addEventListener('load', function () {
 image.addEventListener('load', function () {
-    ctx.drawImage(image, 0, 0);
+    context.drawImage(image, 0, 0);
 }, false);
 // });
--- src/server/main.ts
+++
@@ -1,8 +0,0 @@
-import { MainServer } from './MainServer';
-import { GameSystem } from './GameSystem';
-
-const server = new MainServer();
-server.listen(2000);
-
-const system = new GameSystem(server);
-system.launch();
