82e4fc5fd708e15655e9ee7d2805d710cd30253d
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-21 14:54:59 +0900

    ゲームの状態にプレイヤー達を含むよう変更

--- src/model/GameState.ts
+++ src/model/GameState.ts
@@ -1,3 +1,4 @@
+import { Player } from './Player';
 import { RuleSet } from './RuleSet';
 import { Field } from './Field';
 
@@ -5,10 +6,12 @@ import { Field } from './Field';
  * ゲームの状態
  */
 export class GameState {
+    readonly players: Player[]; // プレイヤー
     readonly rules: RuleSet;    // ルール・セット
     readonly field: Field;
 
-    constructor(rules: RuleSet, field: Field) {
+    constructor(players: Player[], rules: RuleSet, field: Field) {
+        this.players = players;
         this.rules = rules;
         this.field = field;
     }
