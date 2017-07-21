23b248eb2adba85f76792a775e0519a074814fea
kt2029 <kbc16a12@stu.kawahara.ac.jp>
2017-07-21 10:53:44 +0900

    コミットやり直し

--- src/logic/CardEvaluator.ts
+++ src/logic/CardEvaluator.ts
@@ -1,3 +1,33 @@
 /**
  * Created by kt2029 on 2017/07/20.
  */
+import { Card } from '../model/CardModel';
+import { CardComparator } from './CardComparator'
+
+export class CardEvaluator{
+    public Evaluate(cards:Array<Card>){
+        /*
+        *カードの組み合わせは適切か
+         */
+        if (cards.length > 1){
+            /*
+             * 同じ数字の組み合わせ
+             */
+
+            /*
+             * 階段
+             */
+        }
+        /*
+        * 場の状態を引っ張ってくる
+         */
+
+        /*
+         *ローカルルール用処理
+         */
+
+        /*
+        *基本ルールの処理
+         */
+    }
+}
