4a4ea2ec1d9c650068ce8402b6db83cfff194bf8
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2017-07-21 14:53:57 +0900

    ゲームの状態にルールを含むよう変更

--- src/model/GameState.ts
+++ src/model/GameState.ts
@@ -1,13 +1,15 @@
-
+import { RuleSet } from './RuleSet';
 import { Field } from './Field';
 
 /**
  * ゲームの状態
  */
 export class GameState {
+    readonly rules: RuleSet;    // ルール・セット
     readonly field: Field;
 
-    constructor(field: Field) {
+    constructor(rules: RuleSet, field: Field) {
+        this.rules = rules;
         this.field = field;
     }
 }
